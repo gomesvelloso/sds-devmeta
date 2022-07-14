e03faf37ea0c55db11327f3b15cfeb520581d142
Dennis Henrique <gomesvelloso@gmail.com>
2022-07-13 23:09:20 -0300

    Project Created

---
+++ backend/src/main/java/br/com/dhweb/dsmedia/DsmediaApplication.java
@@ -0,0 +1,13 @@
+package br.com.dhweb.dsmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmediaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/dhweb/dsmedia/DsmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dhweb.dsmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
