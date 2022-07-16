1ac8c8ab6ee4728e28f95fb94e6c2c8e2b44b023
Dennis Henrique <gomesvelloso@gmail.com>
2022-07-16 18:47:06 -0300

    Twilio SMS

--- backend/src/main/java/br/com/dhweb/dsmedia/controllers/SaleController.java
+++ backend/src/main/java/br/com/dhweb/dsmedia/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.dhweb.dsmedia.entities.Sale;
 import br.com.dhweb.dsmedia.services.SaleService;
+import br.com.dhweb.dsmedia.services.SmsService;
 
 @RestController
 @RequestMapping(value="/sales")
@@ -17,6 +19,9 @@ public class SaleController {
 	
 	@Autowired //Injetar automaticamente a instância do sale service
 	private SaleService service;
+	
+	@Autowired
+	private SmsService smsService;
 	
 	@GetMapping //Confirgura o metodo para responder via web usando http
 	public Page<Sale> findSales(
@@ -26,6 +31,8 @@ public class SaleController {
 		return service.findSales(minDate,maxDate,pageable);
 	}
 	
-	
-
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id){
+		smsService.sendSms(id);
+	}
 }
