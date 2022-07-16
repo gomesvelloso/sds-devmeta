e3803a0955552d497d82d804749e396455db695d
Dennis Henrique <gomesvelloso@gmail.com>
2022-07-15 23:19:42 -0300

    Date select

--- backend/src/main/java/br/com/dhweb/dsmedia/services/SaleService.java
+++ backend/src/main/java/br/com/dhweb/dsmedia/services/SaleService.java
@@ -1,9 +1,14 @@
 package br.com.dhweb.dsmedia.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.dhweb.dsmedia.entities.Sale;
 import br.com.dhweb.dsmedia.repositories.SaleRepository;
@@ -13,9 +18,15 @@ public class SaleService {
 	
 	@Autowired // Essa anonotation faz a importação automática
 	private SaleRepository repository;
-	public List<Sale> findSales() {
+	
+	public Page<Sale> findSales(String minDate, String maxDate,Pageable pageable) {
 		
-		return repository.findAll(); 
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable); 
 	}
 
 }
